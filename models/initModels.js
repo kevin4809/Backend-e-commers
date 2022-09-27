e9e39de759c6c15a0e6fb7bae1467390a9463b44
kevin4809 <kevinedk33@gmail.com>
2022-09-27 14:26:05 -0500

    Update initModels.js

--- models/initModels.js
+++ models/initModels.js
@@ -1,6 +1,36 @@
 // Models
-const { User } = require('./user.model');
+const { User } = require("./user.model");
+const { Carts } = require("./carts.model");
+const { Orders } = require("./orders.model");
+const { Products } = require("./products.model");
+const { Categories } = require("./categories.model");
+const { ProductImgs } = require("./productImgs.model");
+const { ProductsInCarts } = require("./productsInCar.model");
 
 const initModels = () => {};
+
+User.hasMany(Orders, { foreignKey: "userId" });
+Orders.belongsTo(User);
+
+User.hasOne(Carts, { foreignKey: "userId" });
+Carts.belongsTo(User);
+
+User.hasMany(Products, { foreignKey: "userId" });
+Products.belongsTo(User);
+
+Carts.hasOne(Orders, { foreignKey: "cartId" });
+Orders.belongsTo(Carts);
+
+Categories.hasOne(Products, { foreignKey: "categoryId" });
+Products.belongsTo(Categories);
+
+Products.hasOne(ProductsInCarts, { foreignKey: "productId" });
+ProductsInCarts.belongsTo(Products);
+
+Carts.hasMany(ProductsInCarts, { foreignKey: "cartId" });
+ProductsInCarts.belongsTo(Carts);
+
+Products.hasMany(ProductImgs, { foreignKey: "productId" });
+ProductImgs.belongsTo(Products);
 
 module.exports = { initModels };
